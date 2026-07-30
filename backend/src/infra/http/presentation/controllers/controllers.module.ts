411f079b68920be27fcd8e64db1b607603537f22
manoel-lopes <manoel.lopes.shell@gmail.com>
2026-07-30 19:15:47 -0300

    feat(api): wire log controllers module

--- backend/src/infra/http/presentation/controllers/controllers.module.ts
+++ backend/src/infra/http/presentation/controllers/controllers.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common'
-import { AuthenticateUserController } from './authenticate-user/authenticate-user.controller'
-import { CreateAccountController } from './create-account/create-account.controller'
+import { CreateLogFileController } from './create-log-file/create-log-file.controller'
+import { DashboardController } from './dashboard/dashboard.controller'
+import { GetLogEntryByIdController } from './get-log-entry-by-id/get-log-entry-by-id.controller'
+import { GetLogFileByIdController } from './get-log-file-by-id/get-log-file-by-id.controller'
+import { ListLogFilesController } from './list-log-files/list-log-files.controller'
+import { ListLogsController } from './list-logs/list-logs.controller'
 
 @Module({
   controllers: [
-    AuthenticateUserController,
-    CreateAccountController,
+    CreateLogFileController,
+    ListLogFilesController,
+    GetLogFileByIdController,
+    ListLogsController,
+    GetLogEntryByIdController,
+    DashboardController,
   ],
 })
 export class ControllersModule {}
