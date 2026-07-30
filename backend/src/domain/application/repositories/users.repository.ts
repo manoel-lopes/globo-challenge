b677b306e2947e6d3b6e4a4e2d298d1719c21399
manoel-lopes <manoel.lopes.shell@gmail.com>
2026-07-30 19:14:56 -0300

    refactor(domain): remove user entity and account use cases

--- backend/src/domain/application/repositories/users.repository.ts
+++
@@ -1,9 +0,0 @@
-import type { User, UserProps } from '@/domain/enterprise/entities/user.entity'
-
-export type UsersRepository = {
-  create(user: UserProps): Promise<User>
-  findById(id: string): Promise<User | null>
-  findByEmail(email: string): Promise<User | null>
-}
-
-export const UsersRepository = Symbol('UsersRepository')
