a8184eafb7e44688a2723ff47c83960d3ef7f5a6
manoel-lopes <manoel.lopes.shell@gmail.com>
2026-07-30 19:14:56 -0300

    refactor(api): remove user and account controllers

--- backend/src/infra/http/presentation/controllers/authenticate-user/ports/authenticate-user.protocol.ts
+++
@@ -1,9 +0,0 @@
-import { createZodDto } from 'nestjs-zod'
-import { z } from 'zod'
-
-export const authenticateUserBodySchema = z.object({
-  email: z.email(),
-  password: z.string().min(1),
-})
-
-export class AuthenticateUserBodyDto extends createZodDto(authenticateUserBodySchema) {}
