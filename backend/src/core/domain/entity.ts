2ec14ce1f84169df06993107b947ce3d2a529441
manoel-lopes <manoel.lopes.shell@gmail.com>
2026-08-09 21:37:39 -0300

    refactor: change Entity interface to abstract class with readonly properties
    - Updated Entity from an interface to an abstract class
    - Made id, createdAt, and updatedAt properties readonly
    - Added constructor to initialize id with a UUID v7 if not provided

--- backend/src/core/domain/entity.ts
+++ backend/src/core/domain/entity.ts
@@ -1,5 +1,11 @@
-export interface Entity {
-  id: string
-  createdAt: Date
-  updatedAt?: Date | null
+import { v7 as uuidv7 } from 'uuid'
+
+export abstract class Entity {
+  readonly id: string
+  readonly createdAt: Date
+  readonly updatedAt: Date | null
+
+  protected constructor (id?: string) {
+    this.id = id ?? uuidv7()
+  }
 }
